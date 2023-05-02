f079d2694ecba2ccf329b7edc3104266937b6378
itpazu <itpazu@gmail.com>
2023-05-02 23:39:50 +0300

    completed

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,28 +1,153 @@
 import React from "react";
 import Router, { useRouter } from 'next/router'
+import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Typography from "@mui/material/Typography";
+import Tooltip, { tooltipClasses, TooltipProps } from '@mui/material/Tooltip';
+import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import LoginOutlinedIcon from '@mui/icons-material/LoginOutlined';
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+import TryIcon from '@mui/icons-material/Try';
 import LogoutIcon from '@mui/icons-material/Logout';
 import useUser from '../../src/components/hooks/useUser';
 import useTodos from '../components/hooks/useTodos';
 import axios from "axios";
 
+const PlayGroundToolTip = styled(({ className, ...props }: TooltipProps) => (
+    <Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+
+    [`& .${tooltipClasses.tooltip}`]: {
+        backgroundColor: theme.palette.common.whiteTone,
+        color: 'rgba(0, 0, 0, 0.87)',
+        boxShadow: theme.shadows[2],
+        border: `2px solid ${theme.palette.common.focusColor}`,
+        fontSize: 20,
+    },
+}));
 export default function Layout({ children }: { children?: React.ReactElement }) {
     const { user, mutateUser } = useUser()
     const { mutate: mutateTodos } = useTodos()
 
     const router = useRouter()
-    const goToLogin = () => {
-        Router.push('/login')
+    const redirect = (endpoint: string) => {
+        Router.push(endpoint)
     }
 
     const logOut = () => {
         mutateUser(async () => await axios.get('/api/auth/logout'))
         mutateTodos()
     }
+    console.log(tooltipClasses.tooltip)
+    const renderButtonPerPath = () => {
+        switch (router.pathname) {
+            case "/":
+                return (
+                    <Stack spacing={2} direction="row" justifyContent="flex-end" sx={{ width: '100%' }}>
 
+                        <Button
+                            onClick={() => redirect('/login')}
+                            sx={{ backgroundColor: theme => theme.palette.common.focusColor }}
+                            variant="contained"
+                            endIcon={<LoginOutlinedIcon />}
+                        >
+                            login
+                        </Button>
+                        <Button
+                            onClick={() => redirect('/signup')}
+                            sx={{ backgroundColor: theme => theme.palette.common.focusColor }}
+                            variant="contained"
+                            endIcon={<AppRegistrationIcon />}
+                        >
+                            signUp
+                        </Button>
+                    </Stack>
+
+                )
+            case "/login":
+                return (
+                    <Stack spacing={2} direction="row" justifyContent="space-between" sx={{ width: '100%' }}>
+                        <PlayGroundToolTip
+                            title="play around, without saving your todos"
+                        >
+
+                            <Button
+                                onClick={() => redirect('/')}
+                                sx={{
+                                    color: theme => theme.palette.common.focusColor,
+
+                                    "&:hover": {
+                                        backgroundColor: theme => theme.palette.common.whiteTone
+                                    }
+
+                                }}
+                                variant="text"
+                                endIcon={<TryIcon />}
+                            >
+                                playground
+                            </Button>
+                        </PlayGroundToolTip>
+                        <Button
+                            onClick={() => redirect('/signup')}
+                            sx={{ backgroundColor: theme => theme.palette.common.focusColor }}
+                            variant="contained"
+                            endIcon={<AppRegistrationIcon />}
+                        >
+                            signUp
+                        </Button>
+                    </Stack>
+                )
+            case "/signup":
+                return (
+                    <Stack spacing={2} direction="row" justifyContent="space-between" sx={{ width: '100%' }}>
+
+                        <PlayGroundToolTip
+                            title="play around, without saving your todos"
+                        >
+
+                            <Button
+                                onClick={() => redirect('/')}
+                                sx={{
+                                    color: theme => theme.palette.common.focusColor,
+
+                                    "&:hover": {
+                                        backgroundColor: theme => theme.palette.common.whiteTone
+                                    }
+
+                                }}
+                                variant="text"
+                                endIcon={<TryIcon />}
+                            >
+                                playground
+                            </Button>
+                        </PlayGroundToolTip>
+
+                        <Button
+                            onClick={() => redirect('/login')}
+                            sx={{ backgroundColor: theme => theme.palette.common.focusColor }}
+                            variant="contained"
+                            endIcon={<LoginOutlinedIcon />}
+                        >
+                            login
+                        </Button>
+                    </Stack>
+
+                )
+            default:
+                return (<Button
+                    onClick={logOut}
+                    sx={{ backgroundColor: theme => theme.palette.common.focusColor }}
+                    variant="contained"
+                    endIcon={<LogoutIcon />}
+                >
+                    logout
+                </Button>)
+
+
+
+        }
+    }
     return (
         <Grid
             container
@@ -34,29 +159,8 @@ export default function Layout({ children }: { children?: React.ReactElement }) 
 
         >
             <Grid item container minHeight='10vh' xs={12} alignItems={'center'} >
-
                 <Grid item container xs={12} justifyContent={'flex-end'} >
-                    {router.pathname !== "/login" && (user?.isLoggedIn ?
-                        <Button
-                            onClick={logOut}
-                            sx={{ backgroundColor: theme => theme.palette.common.focusColor }}
-                            variant="contained"
-                            endIcon={<LogoutIcon />}
-                        >
-                            logout
-                        </Button> :
-
-                        <Button
-                            onClick={goToLogin}
-                            sx={{ backgroundColor: theme => theme.palette.common.focusColor }}
-                            variant="contained"
-                            endIcon={<LoginOutlinedIcon />}
-                        >
-                            {router.pathname === '/signup' ? 'login' : 'login / signup'}
-                        </Button>
-
-                    )}
-
+                    {renderButtonPerPath()}
                 </Grid>
                 <Grid item container xs={12} justifyContent={'center'} >
 
